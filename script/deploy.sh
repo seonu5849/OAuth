921ae2646cb37835d69aa499a39907bd0a97c76e
Seonoo <tjsdn22@naver.com>
2024-02-17 02:47:54 +0900

    실제 배포 과정 체험 9

--- script/deploy.sh
+++ script/deploy.sh
@@ -9,16 +9,16 @@ cp $REPOSITORY/zip/*.jar $REPOSITORY/
 
 echo "> 현재 구동 중인 애플리케이션 pid 확인"
 
-CURRENT_PID=$(pgrep -fl OAuth | grep jar | awk '{print $1}') # -- 1
+CURRENT_PID=$(pgrep -fl ${PROJECT_NAME} | grep jar | awk '{print $1}') # -- 1
 
 echo "현재 구동 중인 애플리케이션 pid : $CURRENT_PID"
 
 if [ -z "$CURRENT_PID" ]; then
-    echo "> 현재 구동중인 애플리케이션이 없으므로 종료하지 않습니다."
+  echo "> 현재 구동 중인 애플리케이션이 없으므로 종료하지 않습니다."
 else
-    echo "> kill -15 $CURRENT_PID"
-    kill -15 $CURRENT_PID
-    sleep 5
+  echo "> kill -15 $CURRENT_PID"
+  kill -15 $CURRENT_PID
+  sleep 5
 fi
 
 echo "> 새 애플리케이션 배포"
@@ -34,6 +34,9 @@ chmod +x $JAR_NAME # -- 2
 echo "> $JAR_NAME 실행"
 
 nohup java -jar \
-    -Dspring.config.location=classpath:/application.properties,classpath:/application-real.properties,/home/ubuntu/app/application-oauth.properties,/home/ubuntu/app/application-real-db.properties \
-    -Dspring.profiles.active=real \
-    $JAR_NAME > $REPOSITORY/nohup.out 2>&1 & # -- 3
+  -Dspring.config.location=classpath:/application.properties, # 내부의 application.properties 추가
+  classpath:/application-real.properties, # 내부의 application-real.properties 추가
+  /home/ubuntu/app/application-oauth.properties, # 외부의 application-oauth.properties, real-db 절대경로로 추가
+  /home/ubuntu/app/application-real-db.properties  \
+  -Dspring.profiles.active=real \
+  $JAR_NAME > $REPOSITORY/nohup.out 2>&1 & # -- 3
