ffa4519bc4667d85ab7e23829765080cbf6931d4
Seonoo <tjsdn22@naver.com>
2024-02-17 01:12:50 +0900

    실제 배포 과정 체험 4

--- script/deploy.sh
+++ script/deploy.sh
@@ -34,7 +34,6 @@ chmod +x $JAR_NAME # -- 2
 echo "> $JAR_NAME 실행"
 
 nohup java -jar \
-  -Dspring.config.location=classpath:/application.properties, # 내부의 application.properties 추가
-  classpath:/application-real.properties,/home/ubuntu/app/application-oauth.properties,/home/ubuntu/app/application-real-db.properties  \
+  -Dspring.config.location=classpath:/application.properties,classpath:/application-real.properties,/home/ubuntu/app/application-oauth.properties,/home/ubuntu/app/application-real-db.properties  \
   -Dspring.profiles.active=real \
   $JAR_NAME > $REPOSITORY/nohup.out 2>&1 & # -- 3
