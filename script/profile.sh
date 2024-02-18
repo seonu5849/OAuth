ba0d90e214ab6684fcf3f2bb6d65548f91e4390a
Seonoo <tjsdn22@naver.com>
2024-02-19 00:54:12 +0900

    무중단 배포 테스트 6

--- script/profile.sh
+++ script/profile.sh
@@ -5,7 +5,7 @@
 # 쉬고 있는 profile 찾기: real1이 사용중이면 real2가 쉬고 있고, 반대면 real1이 쉬고 있음
 function find_idle_profile()
 {
-    RESPONSE_CODE=$(curl -s -o /dev/null -w "%{http_code}" http://localhost/profile)
+    RESPONSE_CODE=$(curl -s -o /dev/null -w "%{http_code}" http://localhost/profile) # -- 1
 
     if [ ${RESPONSE_CODE} -ge 400 ] # 400 보다 크면 (즉, 40x/50x 에러 모두 포함)
     then
@@ -16,12 +16,12 @@ function find_idle_profile()
 
     if [ ${CURRENT_PROFILE} == real1 ]
     then
-      IDLE_PROFILE=real2
+      IDLE_PROFILE=real2  # -- 2
     else
       IDLE_PROFILE=real1
     fi
 
-    echo "${IDLE_PROFILE}"
+    echo "${IDLE_PROFILE}"  # -- 3
 }
 
 # 쉬고 있는 profile의 port 찾기
